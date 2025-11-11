f06dba3b96d6265069d1f0fc2ea5d8cbb0014415
victor <vickydollas11@gmail.com>
2025-11-11 10:13:32 +0100

    api issue

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,4 @@ module.exports = defineConfig({
   transpileDependencies: true,
   // 🔑 THE FIX FOR GITHUB PAGES:
   // Sets the base path for assets to match the subdirectory name on GitHub Pages.
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/vue_course_site_purchase/' // <-- REPLACE THIS with your GitHub repository name
-    : '/'
 })
